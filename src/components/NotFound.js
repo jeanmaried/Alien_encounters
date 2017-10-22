d0065b075f67d99d159db395a959463e29fe5b3d
Joseph Dalmasso <me@jodalmasso.com>
2017-10-22 17:32:47 -0400

    removed ufo

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,7 +6,6 @@ const NotFound = () =>
     <div id="home" className="flex direction-row justify-center">
       <Link to="/home" ><div>Home</div></Link>
     </div>
-    <div id='saucer'><img src={require('../ufo.png')} /></div>
     <div className='flex direction-column align-items-center'>
 	    <h1>404 page not found</h1>
 	    <p>We are sorry but the page you are looking for does not exist.</p>
